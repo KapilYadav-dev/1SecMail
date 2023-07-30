b1a97015016e203f82f3a1ab6a4d90943d476790
kapilyadav <kapilyadav@strikergame.com>
2023-07-31 01:21:10 +0530

    adding title to desktop app and optimising resources

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -3,6 +3,7 @@ import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
+        this.window.title="1SecApi ( Unofficial App)"
         MainView()
     }
 }
